01ca806bdb5310680f30ac27ad0910a45ec6f7e4
Nikita Eshkeev <neshkeev@yandex.ru>
2023-10-27 20:39:56 +0300

    Initial placement of the project
    Signed-off-by: Nikita Eshkeev <neshkeev@yandex.ru>

---
+++ antifraud-drools/src/main/java/com/githib/neshkeev/antifraud/Main.java
@@ -0,0 +1,12 @@
+package com.githib.neshkeev.antifraud;
+
+import org.kie.api.*;
+import io.temporal.workflow.WorkflowInterface;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(KieServices.class);
+        System.out.println(WorkflowInterface.class);
+        // System.out.println("Hello, world!");
+    }
+}
