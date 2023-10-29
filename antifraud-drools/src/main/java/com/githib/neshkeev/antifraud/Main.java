2fcc5a4cb996a00f4615545599af3e3abe1eedf1
Nikita Eshkeev <neshkeev@yandex.ru>
2023-10-29 17:31:34 +0300

    Add Drools Rule Engine demo

--- antifraud-drools/src/main/java/com/githib/neshkeev/antifraud/Main.java
+++ antifraud-drools/src/main/java/com/githib/neshkeev/antifraud/Main.java
@@ -1,12 +1,33 @@
 package com.githib.neshkeev.antifraud;
 
-import org.kie.api.*;
-import io.temporal.workflow.WorkflowInterface;
+import org.kie.api.KieServices;
+import org.kie.api.command.Command;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieContainerSessionsPool;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.rule.FactHandle;
+
+import java.util.Set;
 
 public class Main {
+    public enum MessageStatus {
+        HELLO,
+        @SuppressWarnings("unused") GOODBYE
+    }
+
+    public record Message(String message, MessageStatus status) implements Command<Message> { }
+
     public static void main(String[] args) {
-        System.out.println(KieServices.class);
-        System.out.println(WorkflowInterface.class);
-        // System.out.println("Hello, world!");
+        KieServices ks = KieServices.get();
+        KieContainer kc = ks.getKieClasspathContainer();
+        KieContainerSessionsPool pool = kc.newKieSessionsPool(10);
+
+        KieSession kSession = pool.newKieSession("HelloWorldKS");
+        kSession.setGlobal("values", Set.of("Hello, World!"));
+        FactHandle insert = kSession.insert(new Message("Hello, World!", MessageStatus.HELLO));
+        kSession.fireAllRules();
+        kSession.getFactHandles().forEach(System.out::println);
+        System.out.println("After");
+        System.out.println(insert);
     }
 }
